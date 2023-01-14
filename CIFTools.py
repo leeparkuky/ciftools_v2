8bec978cf76da2367db294992db2d4120bffff79
leeparkuky <lee.park@uky.edu>
2023-01-14 23:34:08 +0900

    google colab

--- CIFTools.py
+++ CIFTools.py
@@ -936,9 +936,9 @@ def setup_chrome_driver():
         fp = glob_result[0]
     return fp
     
-def lung_cancer_screening_file_download(location:str):
+def lung_cancer_screening_file_download(chrome_driver_path = None):
     from selenium import webdriver
-    from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver import Options
     from selenium.webdriver.chrome.service import Service
     from selenium.webdriver.common.by import By
     import os
@@ -947,9 +947,21 @@ def lung_cancer_screening_file_download(location:str):
     prefs = {"download.default_directory" : getcwd()}
     chromeOptions.add_experimental_option("prefs",prefs)
     chromeOptions.add_argument(f"download.default_directory={getcwd()}")
-
-    chrome_driver_path = setup_chrome_driver()
+    if chrome_driver_path == None:
+        if os.getenv("COLAB_RELEASE_TAG"):
+            import subprocess
+            subprocess.run(['apt','install','chromium-chromedriver'])
+            chromeOptions.add_argument('--headless')
+            chromeOptions.add_argument('--no-sandbox')
+            chromeOptions.add_argument('--disable-dev-shm-usage')
+            chromeOptions.add_argument("window-size=1200x600")
+            chrome_driver_path = '/usr/lib/chromium-browser/chromedriver'
+            print('google chrome driver is ready')
+        else:
+            chrome_driver_path = setup_chrome_driver()
+    print('cp1')
     driver = webdriver.Chrome(executable_path=chrome_driver_path, options=chromeOptions)
+    print('cp2')
     url = 'https://report.acr.org/t/PUBLIC/views/NRDRLCSLocator/LCSLocator?:embed=y&:showVizHome=no&:host_url=https%3A%2F%2Freport.acr.org%2F&:embed_code_version=3&:tabs=no&:toolbar=no&:showAppBanner=no&:display_spinner=no&:loadOrderID=0'
     driver.get(url);  time.sleep(10)
     state = driver.find_elements(By.CLASS_NAME, 'tabComboBoxButtonHolder')[2]; state.click(); time.sleep(10)
